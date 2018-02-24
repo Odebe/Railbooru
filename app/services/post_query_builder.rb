afaf31cb8f253b2b4e9e7d9d223bf69a55cc7b67
Odebe <derpiranha@gmail.com>
2018-02-24 17:48:38 +0300

    silly hacks

--- app/services/post_query_builder.rb
+++ app/services/post_query_builder.rb
@@ -19,30 +19,51 @@ class PostQueryBuilder
   def filter_by_tags(scope, tags_string = nil)
     tags = get_tags(tags_string)
     return scope unless tags
-    tags, aliases_count = filter_by_aliases(tags)
+    tags, aliases_count = add_aliases_to(tags)
+    #puts tags.count, aliases_count
+    general_count = 
+              if tags.count == aliases_count
+                1
+              else
+                tags.count-aliases_count
+              end
     scope
         .joins(:tags)
         .where(tags: {name: tags})
         .group('posts.id')
-        .having('count(*) = ?', tags.count-aliases_count)
+        .having('count(*) = ?', general_count)
   end
-  def filter_by_aliases(tags)
-    return unless tags
+  def add_aliases_to(tags)
+    new_tags = Array.new
     aliases_count = 0
-    tags.map do |tag_name|
-      new_name = get_alias(tag_name)
-      next unless new_name
-      tags << new_name
-      aliases_count += 1
+    tags.each do |tag_name|
+      new_tags << tag_name
+      tag = Tag.find_by(name: tag_name)
+      next unless tag
+      if tag.aliases.any?
+        tag.aliases.each do |ali|
+          new_tags << ali.name unless new_tags.include? ali.name
+          aliases_count += 1
+        end
+      else
+        new_tag = Tag.joins(:tag_aliases).where(tag_aliases: {alias_id: tag.id})
+        next unless new_tag.first
+        new_tags << new_tag.first.name
+        aliases_count += 1
+        new_tag.first.aliases.each do |ali|
+          new_tags << ali.name unless new_tags.include? ali.name
+          aliases_count += 1
+        end
+      end
     end
-    return tags, aliases_count
+    return new_tags, aliases_count
   end
-  def get_alias(tag_name)
-    tag = Tag.find_by(name: tag_name)
+  def get_alias(tag)
+    #tag = Tag.find_by(name: tag_name)
     return unless tag
     tag_alias = TagAlias.find_by(alias_id: tag.id)# if tag
     return unless tag_alias
-    Tag.find(tag_alias.tag_id).name 
+    Tag.find(tag_alias.tag_id)
   end
   def get_tags(tags_string)
     tags_string ? tags_string.split(" ").uniq : nil
